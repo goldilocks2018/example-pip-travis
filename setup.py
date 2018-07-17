ee92be714f60f89835eb35e413f40cd858ac05f3
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 12:03:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
                         "tlslite-ng==0.5.1", "aodh==2.0.0.0b2", "pyanyapi==0.2.1", "graphite-web==0.9.6",
                         "diffoscope==76", "priority==1.1.1", "django-anymail==1.2.1", "mysql-connector==2.1.3",
                         "tripleo-image-elements==0.8.1", "jsnapy==1.2.1", "tile-generator==5.0.7",
-                        "owlmixin==2.0.0a6", "Products.kupu==1.4.16", "suricata-update==0.1.0b1",
+                        "owlmixin==2.0.0a6", "Products.kupu==1.4.16",
                         "ansible==1.8.3", "ansible-vault==1.0.4", "archmage==0.3", "askbot==0.7.37",
                         "asyncssh==1.5.1", "Bcfg2==1.2.0", "Beaker==1.6.4", "bleach==2.1", "bottle==0.11.1",
                         "celery==2.4.0", "ceph-deploy==1.5.22", "confire==0.2.0",
