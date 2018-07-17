00b8010caa911ad2c71dc14bfc2582e5bd1be9b5
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 14:36:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
                         "salt==0.10.3", "Sanic==0.5.0", "Scrapy==0.24.6", "setuptools==0.6c9", "sleekxmpp==1.3.1", 
                         "SOAPpy==0.12.5", "sosreport==3.2", "supervisor==3.1.2", "swauth==1.2.0", "swift==1.0.2", "tablib==0.11.4",
                         "textract==1.4.0", "tlslite==0.4.9", "tornado==1.2", "tqdm==4.4.1", "tripleo-heat-templates==0.6.4",
-                        "tryton==3.8.7", "trytond==3.4.7", "tweepy==1.4", "txAWS==0.2.3.1", "urllib3==1.18", "uWSGI==1.9.12",
+                        "tryton==3.8.7", "trytond==3.4.7", "tweepy==1.4", "txAWS==0.2.3.1", "urllib3==1.18",
                         "Werkzeug==0.11"],
     entry_points={
         'console_scripts': [
