fb4e102e8d7b7fde145331b6ac81bcf11410ab13
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 12:52:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
                         "gunicorn==19.4.5", "Kotti==0.5.0a6", "markdown2==2.3.5", "moin==1.9.5",
                         "oauth2==1.5.153", "pycares==1.0.0", "pymongo==2.4.2", "PyYAML==3.11",
                         "qutebrowser==0.6.1", "recurly==2.6.0", "rsa==3.1.1", "topydo==0.13",
-                        "websockets==4.0", "Zope2==2.12.2", "zuul==3.0.1", "python-qpid-proton==0.12.0",
+                        "websockets==4.0", "Zope2==2.12.2", "zuul==3.0.1",
                         "apache-libcloud==0.13.2", "oslo.middleware==2.0.0",
                         "Products.CMFEditions==2.0b7", "aiohttp-session==1.2.1", "ovs==2.7.0",
                         "manila-ui==2.2.0", "django-tastypie==0.9.9",
