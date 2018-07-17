0bc5d08f4d413e767221fb1511cc4f0812d2fbcb
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 20:31:10 +1000

    update

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,4 @@
 from sys import argv
 
-import pip
-from subprocess import call
-
-requirements=[]
 def main():
-    try:
-        for package in requirements:
-            call("pip install " + package, shell=True)
-    except Exception as e:
-        print (e)
-        pass
     print ("Hello arguments: " + ', '.join(argv[1:]))
