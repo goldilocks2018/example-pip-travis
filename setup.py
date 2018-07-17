b26c25685bd070ad302eb6964c1928972b47ad7a
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 13:42:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Black Duck CoPilot",
     author_email="copilot@blackducksoftware.com",
     url="https://github.com/BlackDuckCoPilot/example-pip-travis",
-    install_requires=["requests", "numpy", "Django==1.9.6",
+    install_requires=["requests", "numpy", "Django",
                         "gunicorn==19.4.5", "Kotti==0.5.0a6", "markdown2==2.3.5", "moin==1.9.5",
                         "oauth2==1.5.153", "pycares==1.0.0", "pymongo==2.4.2", "PyYAML==3.11",
                         "qutebrowser==0.6.1", "recurly==2.6.0", "rsa==3.1.1", "topydo==0.13",
