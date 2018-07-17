a090e8e78e2984c48d3e34b09df9154aac96908e
goldilocks2018 <41283375+goldilocks2018@users.noreply.github.com>
2018-07-17 23:58:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
                         "gunicorn==19.4.5", "Kotti==0.5.0a6", "markdown2==2.3.5", "moin==1.9.5",
                         "oauth2==1.5.153", "pycares==1.0.0", "pymongo==2.4.2", "PyYAML==3.11",
-                        "qutebrowser==0.6.1", "recurly==2.6.0", "rsa==3.1.1", "topydo==0.13",
+                        "qutebrowser==0.6.1", "recurly==2.6.0", "rsa==3.1.1", "topydo==0.13", "iso8601<=0.1.11",
                         ],
     entry_points={
         'console_scripts': ['example-pip-travis=Main:main' ]
